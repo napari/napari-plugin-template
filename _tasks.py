e04df9dcffb0d1c113621e12a03b0dc64c8f5849
Tim Monko <timmonko@gmail.com>
2026-03-26 21:45:27 -0500

    update tasks and readme to remove folder mention

--- _tasks.py
+++ _tasks.py
@@ -186,9 +186,7 @@ Your plugin template is ready!  Next steps:
 
     msg += """
     5. Read the README for more info: https://github.com/napari/napari-plugin-template
-    6. We've provided a template description for your plugin page on the napari hub at `.napari-hub/DESCRIPTION.md`.
-    You'll likely want to edit this before you publish your plugin.
-    7. Consider customizing the rest of your plugin metadata for display on the napari hub:
+    6. Consider customizing the rest of your plugin metadata for display on the napari hub:
     https://github.com/chanzuckerberg/napari-hub/blob/main/docs/customizing-plugin-listing.md
     """
     return msg
